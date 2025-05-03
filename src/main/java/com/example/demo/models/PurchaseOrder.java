e220ce53b0a4251ddeedc4768bf0896ee0703ddb
soaalice <amboararampanana@gmail.com>
2025-05-03 13:42:48 +0300

    filter status

--- src/main/java/com/example/demo/models/PurchaseOrder.java
+++ src/main/java/com/example/demo/models/PurchaseOrder.java
@@ -1,6 +1,7 @@
 package com.example.demo.models;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
 
 import lombok.Data;
 
@@ -10,5 +11,7 @@ public class PurchaseOrder {
     private String name;
     private String supplier;
     private String status;
+    @JsonProperty("advance_payment_status")
+    private String advancedPaymentStatus;
     private String total;
 }
--- src/main/java/com/example/demo/services/PurchaseOrderService.java
+++ src/main/java/com/example/demo/services/PurchaseOrderService.java
@@ -19,7 +19,7 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 public class PurchaseOrderService {
     
     public List<PurchaseOrder> getPurchaseOrdersBySupplier(String supplierId, String sessionId) throws Exception {
-        String fields = "[\"name\", \"supplier\", \"status\", \"total\"]";
+        String fields = "[\"name\", \"supplier\", \"status\", \"advance_payment_status\", \"total\"]";
         String filters = "[[\"supplier\", \"=\", \"" + supplierId + "\"]]";
         String url = ErpApiConfig.ERP_URL_RESOURCE + "/Purchase Order?fields=" + fields + "&filters=" + filters;
         RestTemplate restTemplate = new RestTemplate();
@@ -45,4 +45,72 @@ public class PurchaseOrderService {
             .map(data -> objectMapper.convertValue(data, PurchaseOrder.class))
             .toList();
     }
+
+    public List<PurchaseOrder> getPurchaseOrdersByStatus(String status, String sessionId) throws Exception {
+        String fields = "[\"name\", \"supplier\", \"status\", \"advance_payment_status\", \"total\"]";
+        String filters = "[[\"status\", \"=\", \"" + status + "\"]]";
+        String url = ErpApiConfig.ERP_URL_RESOURCE + "/Purchase Order?fields=" + fields + "&filters=" + filters;
+        RestTemplate restTemplate = new RestTemplate();
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.set("Cookie", sessionId);
+        headers.setAccept(List.of(MediaType.APPLICATION_JSON));
+
+        HttpEntity<String> request = new HttpEntity<>(headers);
+
+        ResponseEntity<Map> response = restTemplate.exchange(
+            url,
+            HttpMethod.GET,
+            request,
+            Map.class
+        );
+
+        ObjectMapper objectMapper = new ObjectMapper();
+
+        List<Map<String, Object>> rawSuppliers = (List<Map<String, Object>>) response.getBody().get("data");
+
+        return rawSuppliers.stream()
+            .map(data -> objectMapper.convertValue(data, PurchaseOrder.class))
+            .toList();
+    }
+
+    public List<PurchaseOrder> getPurchaseOrdersByStatuses(String[] statuses, String sessionId) throws Exception {
+        String fields = "[\"name\", \"supplier\", \"status\", \"advance_payment_status\", \"total\"]";
+
+        StringBuilder filtersBuilder = new StringBuilder("[");
+        for (int i = 0; i < statuses.length; i++) {
+            filtersBuilder.append("[\"status\", \"=\", \"").append(statuses[i]).append("\"]");
+            if (i < statuses.length - 1) {
+                filtersBuilder.append(", \"or\", ");
+            }
+        }
+        filtersBuilder.append("]");
+
+        String filters = filtersBuilder.toString();
+        String url = ErpApiConfig.ERP_URL_RESOURCE + "/Purchase Order?fields=" + fields + "&filters=" + filters;
+
+        RestTemplate restTemplate = new RestTemplate();
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.set("Cookie", sessionId);
+        headers.setAccept(List.of(MediaType.APPLICATION_JSON));
+
+        HttpEntity<String> request = new HttpEntity<>(headers);
+
+        ResponseEntity<Map> response = restTemplate.exchange(
+            url,
+            HttpMethod.GET,
+            request,
+            Map.class
+        );
+
+        ObjectMapper objectMapper = new ObjectMapper();
+
+        List<Map<String, Object>> rawSuppliers = (List<Map<String, Object>>) response.getBody().get("data");
+
+        return rawSuppliers.stream()
+            .map(data -> objectMapper.convertValue(data, PurchaseOrder.class))
+            .toList();
+    }
+
 }
